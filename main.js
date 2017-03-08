7da6c1d70682a89059e2deba41cf9f64fb935f9f
Meghan Debity <mdebity@gmail.com>
2017-03-07 21:02:22 -0600

    added JSON and callbacks

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 console.log("hello");
+
+
+var dogFoodContainer = document.getElementById("dogFoodContainer");
+
+function sendToDom(xhrData){
+	var dogFoodString = "";
+	var currentFood;
+	for(var i=0; i<xhrData.dog_brands.length; i++){
+		currentFood = xhrData.dog_brands[i];
+
+		dogFoodString +=
+		dogFoodString +=
+		dogFoodString +=
+		dogFoodString +=
+	}
+	dogFoodContainer.innerHTML = dogFoodString;
+}
+
+function executeThisCodeAfterFileIsLoaded(){
+	var data = JSON.parse(this.responseText);
+	sendToDom(data);
+}
+
+function executeThisCodeAfterFileFails(){
+	alert("BOOO");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "main.json");
+myRequest.send();
+
+
+
+
+
+
+
+
