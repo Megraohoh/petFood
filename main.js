07955a3d3fa6be4c5f787610dfc30dee76ca2dcb
Meghan Debity <mdebity@gmail.com>
2017-03-09 19:59:42 -0600

    dog food portion working

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-console.log("hello");
-
 
 var dogFoodContainer = document.getElementById("dogFoodContainer");
 
 function sendToDom(xhrData){
 	var dogFoodString = "";
 	var currentFood;
-	for(var i=0; i<xhrData.dog_brands.length; i++){
-		currentFood = xhrData.dog_brands[i];
 
-		dogFoodString +=
-		dogFoodString +=
-		dogFoodString +=
-		dogFoodString +=
+	for(var i=0; i<xhrData.dog_brands.length; i++){
+		currentFood = xhrData.dog_brands;
+		dogFoodString += `<h3>${currentFood[i].name}</h3>`;
+	
+		for(var j=0; j<xhrData.dog_brands[i].types.length; j++){
+			var currentFoodType = xhrData.dog_brands[i].types[j].type;
+			// dogFoodString += `<h3>${currentFoodType}</h3>`;
+			dogFoodString += `<p>Food type is ${currentFoodType}</p>`
+
+			for(var k=0; k<xhrData.dog_brands[i].types[j].volumes.length; k++){
+				var currentSizePrice = xhrData.dog_brands[i].types[j].volumes[k];
+
+				dogFoodString += `<p>The ${currentSizePrice.name} weight is ${currentSizePrice.price}</p>`;
+				// dogFoodString += `<p>${currentSizePrice.price}</p>`;
+			}
+
+		}
+
+
 	}
+
 	dogFoodContainer.innerHTML = dogFoodString;
 }
 
 function executeThisCodeAfterFileIsLoaded(){
 	var data = JSON.parse(this.responseText);
 	sendToDom(data);
+	console.log(data);
 }
 
 function executeThisCodeAfterFileFails(){
@@ -29,8 +42,24 @@ function executeThisCodeAfterFileFails(){
 var myRequest = new XMLHttpRequest();
 myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
-myRequest.open("GET", "main.json");
+myRequest.open("GET", "dogfood.json");
 myRequest.send();
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
 
 
 
